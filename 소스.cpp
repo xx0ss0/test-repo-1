0dac002055d711553c51d92dd634f00f022a6c75
Yewon Shin <rac8793@naver.com>
2023-03-18 13:53:00 +0900

    print everything in one line

--- 소스.cpp
+++ 소스.cpp
@@ -6,8 +6,7 @@
 using namespace std;
 
 void main() {
-	string greeting = "Hello world!\n";     //greeting 문자열 변수에 "Hello world!" 할당
-	string time = "It's lunch time now.\n"; //time 문자열 변수에 "It's lunch time now." 할당
-        cout << "--Start--\n";                  //"--Start--" 문자열 출력 
-	cout << greeting << time;               //"Hello world!"와 "It's lunch time now." 문자열 출력
+	string greeting = "Hello world!\n";         //greeting 문자열 변수에 "Hello world!" 할당
+	string time = "It's lunch time now.\n";     //time 문자열 변수에 "It's lunch time now." 할당
+	cout << "--Start--\n" << greeting << time;  //"--Start--", "Hello world!", "It's lunch time now." 문자열 출력
 }
