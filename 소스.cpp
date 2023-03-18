8f8435a7e1733f63ae7c2fcc68bf9c4d0b54330b
ywshin02 <127467137+ywshin02@users.noreply.github.com>
2023-03-18 12:14:14 +0900

    주석 추가 및 수정

--- 소스.cpp
+++ 소스.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 
 void main() {
-	string greeting = "hello world\n";
-	string time = "It's lunch time now.\n";
-	printf("\n");            //줄바꿈     
-	cout << greeting; //"hello world" 문자열 출력
-	cout << time;  //"It's lunch time now." 문자열 출력
+	string greeting = "hello world\n";      //greeting 문자열 변수에 "hello world" 할당
+	string time = "It's lunch time now.\n"; //time 문자열 변수에 "It's lunch time now." 할당
+	printf("\n");                           //줄바꿈     
+	cout << greeting;                       //"hello world" 문자열 출력
+	cout << time;                           //"It's lunch time now." 문자열 출력
 }
